febf7d1f6a5a9a45ae102a65ca95a91a463ad35c
Emre Özdil <emreozdil@icloud.com>
2017-12-12 23:39:01 +0300

    Add SwiftLint

--- Audible/PageCell.swift
+++ Audible/PageCell.swift
@@ -9,31 +9,31 @@
 import UIKit
 
 class PageCell: UICollectionViewCell {
-    
+
     var page: Page? {
         didSet {
             guard let page = page else {
                 return
             }
-            
+
             imageView.image = UIImage(named: page.imageName)
-            
+
             let color = UIColor(white: 0.2, alpha: 1)
-            
+
             let attributedText = NSMutableAttributedString(string: page.title, attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 20, weight: UIFont.Weight.medium), NSAttributedStringKey.foregroundColor: color])
-            
+
             attributedText.append(NSAttributedString(string: "\n\n\(page.message)", attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 14), NSAttributedStringKey.foregroundColor: color]))
-            
+
             let paragraphStyle = NSMutableParagraphStyle()
             paragraphStyle.alignment = .center
-            
+
             let length = attributedText.string.count
             attributedText.addAttribute(NSAttributedStringKey.paragraphStyle, value: paragraphStyle, range: NSRange(location: 0, length: length))
-            
+
             textView.attributedText = attributedText
         }
     }
-    
+
     let imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "page1")
@@ -41,50 +41,50 @@ class PageCell: UICollectionViewCell {
         imageView.clipsToBounds = true
         return imageView
     }()
-    
+
     let textView: UITextView = {
         let textView = UITextView()
         textView.text = "SAMPLE TEXT FOR NOW"
         textView.isEditable = false
         return textView
     }()
-    
+
     let lineSeperatorView: UIView = {
         let view = UIView()
         view.backgroundColor = UIColor(white: 0.9, alpha: 1)
         return view
     }()
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+
         setupView()
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     func setupView() {
-        
+
         addSubview(imageView)
         addSubview(textView)
         addSubview(lineSeperatorView)
-        
+
         imageView.snp.makeConstraints { (make) in
             make.top.equalToSuperview()
             make.left.equalToSuperview()
             make.right.equalToSuperview()
             make.height.equalToSuperview().multipliedBy(0.7)
         }
-        
+
         textView.snp.makeConstraints { (make) in
             make.top.equalTo(imageView.snp.bottom)
             make.left.equalToSuperview().offset(16)
             make.bottom.equalToSuperview()
             make.right.equalToSuperview().offset(-16)
         }
-        
+
         lineSeperatorView.snp.makeConstraints { (make) in
             make.top.equalTo(imageView.snp.bottom)
             make.left.equalToSuperview()
@@ -92,5 +92,5 @@ class PageCell: UICollectionViewCell {
             make.height.equalTo(1)
         }
     }
-    
+
 }
--- Audible/ViewController.swift
+++ Audible/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
-    
+
     lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
@@ -22,20 +22,20 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         collectionView.isPagingEnabled = true
         return collectionView
     }()
-    
+
     let cellId = "cellId"
-    
+
     let pages: [Page] = {
         let firstPage = Page(title: "Share a great listen", message: "It's free to send your books to the people in your life. Every recipent's first book is on us.", imageName: "page1")
         let secondPage = Page(title: "Send from your library", message: "Tap the More menu next to any book. Choose \"Send this Book\"", imageName: "page2")
-        
+
         let thirdPage = Page(title: "Send from the player", message: "Tap the More menu in the upper corner. Choose \"Send this Book\"", imageName: "page3")
         return [firstPage, secondPage, thirdPage]
     }()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         view.addSubview(collectionView)
         collectionView.snp.makeConstraints { (make) in
             make.top.equalToSuperview()
@@ -44,13 +44,13 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
             make.right.equalToSuperview()
         }
         collectionView.register(PageCell.self, forCellWithReuseIdentifier: cellId)
-        
+
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return pages.count
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! PageCell
         let page = pages[indexPath.item]
@@ -58,9 +58,7 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         return cell
     }
 
-    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: view.frame.width, height: view.frame.height)
     }
 }
-
